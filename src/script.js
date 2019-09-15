d79aa3d6f91363a3a04e588076a7d6673a618565
danchitnis <danchitnis@users.noreply.github.com>
2019-09-15 03:33:33 +0100

    added random walk

--- src/script.js
+++ src/script.js
@@ -11,6 +11,7 @@ var noUiSlider = require("nouislider");
 var canv = document.getElementById("my_canvas");
 var devicePixelRatio = window.devicePixelRatio || 1;
 var num = Math.round(canv.clientWidth * devicePixelRatio);
+//let num=1000;
 var line_color1 = new webGLplot_2.color_rgba(1, 1, 0, 1);
 var line_color2 = new webGLplot_2.color_rgba(1, 0, 0, 1);
 var lg1 = new webGLplot_3.lineGroup(line_color1);
@@ -27,7 +28,9 @@ var phi_delta = 1;
 for (var i = 0; i < num; i++) {
     //set x to -num/2:1:+num/2
     lg1.xy.set(i, 0, 2 * i / num - 1);
+    lg1.xy.set(i, 1, 0);
     lg2.xy.set(i, 0, 2 * i / num - 1);
+    lg2.xy.set(i, 1, 0);
 }
 var phi = 0;
 //sliders
@@ -88,12 +91,26 @@ slider_phid.noUiSlider.on("update", function (values, handle) {
     document.getElementById("display_phid").innerHTML = phi_delta.toString();
 });
 setInterval(function () {
-    for (var i = 0; i < num; i++) {
-        var y1 = Math.sin(i * freq * Math.PI / 100 + phi) + Math.random() * Namp / 1;
-        lg1.xy.set(i, 1, 0.9 * 0.5 * Samp * y1 + 0.25);
-        var y2 = Math.sin(i * freq * Math.PI / 100 + phi) + Math.random() * Namp / 1;
-        lg2.xy.set(i, 1, 0.9 * 0.5 * Samp * y2 - 0.25);
-    }
-    phi = phi + phi_delta * 0.5;
+    random_walk();
     wglp.update();
-}, 16.67 * 3);
+}, 16.67 * 1);
+function random_walk() {
+    for (var i = 0; i < num - 1; i++) {
+        lg1.xy.set(i, 1, lg1.xy.get(i + 1, 1));
+        lg2.xy.set(i, 1, lg2.xy.get(i + 1, 1));
+    }
+    var y1 = lg1.xy.get(num - 1, 1) + 0.02 * (Math.round(Math.random()) - 0.5);
+    var y2 = lg2.xy.get(num - 1, 1) + 0.02 * (Math.round(Math.random()) - 0.5);
+    lg1.xy.set(num - 1, 1, y1);
+    lg2.xy.set(num - 1, 1, y2);
+}
+/*function sinwave() {
+  for (let i=0; i<num; i++) {
+    let y1= Math.sin(i*freq*Math.PI/100 + phi) + Math.random()*Namp/1;
+    lg1.xy.set(i,1, 0.9*0.5*Samp*y1+0.25);
+  
+    let y2= Math.sin(i*freq*Math.PI/100 + phi) + Math.random()*Namp/1;
+    lg2.xy.set(i,1, 0.9*0.5*Samp*y2-0.25);
+  }
+  phi = phi + phi_delta*0.5;
+}*/ 
--- src/script.ts
+++ src/script.ts
@@ -14,6 +14,7 @@ let canv = <HTMLCanvasElement>document.getElementById("my_canvas");
 
 let devicePixelRatio = window.devicePixelRatio || 1;
 let num = Math.round(canv.clientWidth * devicePixelRatio);
+//let num=1000;
 
 
 let line_color1 = new color_rgba(1,1,0,1);
@@ -40,7 +41,9 @@ let phi_delta=1;
 for (let i=0; i<num; i++) {
    //set x to -num/2:1:+num/2
    lg1.xy.set(i, 0, 2*i/num-1);
+   lg1.xy.set(i, 1, 0);
    lg2.xy.set(i, 0, 2*i/num-1);
+   lg2.xy.set(i, 1, 0);
 }
 
 
@@ -120,8 +123,30 @@ slider_Samp.noUiSlider.on("update", function(values, handle) {
 
 
 
-
 setInterval(function () {
+
+  random_walk();
+  
+  wglp.update();
+   
+}, 16.67*1);
+
+
+function random_walk() {
+  for (let i=0; i<num-1; i++) {
+    lg1.xy.set(i,1, lg1.xy.get(i+1,1));
+    lg2.xy.set(i,1, lg2.xy.get(i+1,1));
+    
+  }
+  let y1 = lg1.xy.get(num-1,1) + 0.02 * (Math.round(Math.random()) -0.5);
+  let y2 = lg2.xy.get(num-1,1) + 0.02 * (Math.round(Math.random()) -0.5);
+
+  lg1.xy.set(num-1,1,y1);
+  lg2.xy.set(num-1,1,y2);
+
+}
+
+/*function sinwave() {
   for (let i=0; i<num; i++) {
     let y1= Math.sin(i*freq*Math.PI/100 + phi) + Math.random()*Namp/1;
     lg1.xy.set(i,1, 0.9*0.5*Samp*y1+0.25);
@@ -130,7 +155,4 @@ setInterval(function () {
     lg2.xy.set(i,1, 0.9*0.5*Samp*y2-0.25);
   }
   phi = phi + phi_delta*0.5;
-  
-  wglp.update();
-   
-}, 16.67*3);
+}*/
